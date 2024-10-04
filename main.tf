9a42de30bf432e7291a18f3546e1b2659c49383e
esurendrababu <112386357+esurendrababu@users.noreply.github.com>
2024-10-04 14:47:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
-resource "aws_instance" {
-ami = "ami-00f251754ac5da7f0"
-instance_type = "t2.micro"
-tags = {
-Name = "terraform_server"
+
+resource "aws_instance" "example" {
+  ami           = "ami-00f251754ac5da7f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform_server"
+  }
 }
-}
+
